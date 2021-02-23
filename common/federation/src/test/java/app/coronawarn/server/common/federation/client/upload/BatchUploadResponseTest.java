b5f7fb66aaebbfee03fa632229e2714331617514
Hafsa Aoutir <hafsaa@kth.se>
2021-02-23 14:47:29 +0100

    add imports

--- common/federation/src/test/java/app/coronawarn/server/common/federation/client/upload/BatchUploadResponseTest.java
+++ common/federation/src/test/java/app/coronawarn/server/common/federation/client/upload/BatchUploadResponseTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
+import java.util.Arrays; 
+import java.util.List;
 
 class BatchUploadResponseTest {
 
